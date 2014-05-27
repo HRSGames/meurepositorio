1d70e15f0d0aa2e6c724d09998aeb59928ed4b0d
higor_scarduelli <higor_scarduelli@10.1.48.131>
2014-05-27 14:44:10 -0300

    inicialização de um projeto

---
+++ src/novoproj/Novoproj.java
@@ -0,0 +1,13 @@
+
+package novoproj;
+
+import javax.swing.JOptionPane;
+
+
+public class Novoproj {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"FMS_FML");
+    }
+}
